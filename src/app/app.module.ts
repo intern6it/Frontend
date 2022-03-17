1d909db06c7ac9fe3731b9c4e5abe4838e91c4ce
intern6it <interns.it6@onlinepsbloans.com>
2022-03-17 14:57:51 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './header/home/home.component';
+import { NavBarComponent } from './header/nav-bar/nav-bar.component';
+import { FooterComponent } from './footers/footer/footer.component';
+import { AboutComponent } from './header/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
